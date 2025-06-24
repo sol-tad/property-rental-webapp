7a91912b6d52a2fe32903abfbe381ed98d77ae64
sol-tad <soltad65@gmail.com>
2025-06-24 17:25:54 +0300

    project setup

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className=" bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">HomePage</h1>
+    </div>
+  );
+};
+
+export default HomePage;
