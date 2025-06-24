89e3c906f7d81809cd1b0375aebc970face0d5ae
sol-tad <soltad65@gmail.com>
2025-06-24 17:44:39 +0300

    add assets for the project and setup meta data

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,18 @@
-import '../assets/styles/globals.css';
+import "../assets/styles/globals.css";
+export const metadata = {
+  title: "Property Pulse",
+  keywords: "real estate, property, rental, buy, sell, invest",
+  description: "Find the perfect rental property",
+};
+import React from "react";
+const MainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
 
-const MainLayout = ({children}) => {
-  return <html>
-    <body>
-      <main>
-        {children}
-      </main>
-    </body>
-  </html>;
-}
- 
 export default MainLayout;
