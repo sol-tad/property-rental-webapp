dc425068bd38415b60285a8cd02cf1ad29e7e965
sol-tad <soltad65@gmail.com>
2025-06-30 20:15:49 +0300

    some update

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,14 +12,13 @@ export const metadata = {
 const MainLayout = ({ children }) => {
   return (
     <AuthProvider>
-
-    <html lang="en" >
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer/>
-      </body>
-    </html>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
     </AuthProvider>
   );
 };
