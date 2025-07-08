1816bfeeacd8f88b6d932ef7ddcd5c681103da9e
sol-tad <soltad65@gmail.com>
2025-07-08 11:53:42 +0300

    add map location for a property

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -6,7 +6,7 @@ import {
   FaCheck,
   FaMapMarker,
 } from 'react-icons/fa';
-// import PropertyMap from '@/components/PropertyMap';
+import PropertyMap from '@/components/PropertyMap';
 
 const PropertyDetails = ({ property }) => {
   return (
@@ -91,7 +91,7 @@ const PropertyDetails = ({ property }) => {
         </ul>
       </div>
       <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
-        {/* <PropertyMap property={property} /> */}
+        <PropertyMap property={property} />
       </div>
     </main>
   );
